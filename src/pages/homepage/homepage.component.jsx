b4384585eb0d5b03463ded41d087445859a2b7cf
ibra-kdbra <git-repo-develop@outlook.com>
2023-06-03 22:58:35 +0500

    contain styling previously then component for the home page

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Directory from "../../components/directory/directory.component";
+
+import {Home} from './homepage.styles.jsx';
+
+const HomePage = ()=>(
+    <Home>
+    <Directory/>
+    </Home>
+    
+)
+export default HomePage;
---
+++ src/pages/homepage/homepage.styles.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Home = styled.div`
+      display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 20px 80px;
+`
+
+// .homepage {
+//     display: flex;
+//     flex-direction: column;
+//     align-items: center;
+//     padding: 20px 80px;
+//   }
+  
